671613c00ef768f3e9e2581f9fa0efdf5e59b841
ComaN10 <carlosmanuel2003@gmail.com>
2023-03-21 16:38:42 +0000

    Initial commit

---
+++ Projeto_1_EDA/Horta.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+#include <iostream>
+#include "menu.h"
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main() {
+	gestao();
+}
---
+++ Projeto_1_EDA/menu.cpp
@@ -0,0 +1,49 @@
+using namespace std;
+#include <iostream>
+#include "menu.h"
+
+void gestao() {
+	char opçao;
+	cout << "======== Bem Vindo ao Menu Gestor ========" << endl;
+	cout << "(1) - Colher produto" << endl;
+	cout << "(2) - Atualizar tempo de rega" << endl;
+	cout << "(3) - Fertilizacao" << endl;
+	cout << "(4) - Gravar Plantacao " << endl;
+	cout << "(5) - Carregar Plantacao" << endl;
+	cout << "(6) - Imprimir Plantacao" << endl;
+	cout << "(7) - Criar nova area " << endl;
+	cout << "(8) - Mostrar registo de agricultor" << endl;
+	cout << "(9) - Alterar area" << endl;
+	cout << "(0) - Voltar" << endl;
+	cout << "Selecione a sua opcao" << endl;
+	cin >> opçao;
+	//criaçao do menu
+	switch (opçao) {
+	case '1':
+		break;
+	case '2':
+		break;
+	case '3':
+		break;
+	case '4':
+		break;
+	case '5':
+		break;
+	case '6':
+		break;
+	case '7':
+		break;
+	case '8':
+		break;
+	case '9':
+		break;
+	case '0':
+		gestao();
+		break;
+
+
+	}
+	
+
+}
+	
